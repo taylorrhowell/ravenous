56d7c7d99a4b3e8bdf95c2985f3e8a314579c214
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-06-08 13:47:14 -0700

    update Comps after checking part 1 solution

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -21,14 +21,16 @@ class Business extends React.Component {
                 </div>
                     <h2>{business.name}</h2>
                 <div className="Business-information">
-                    <p>{business.address}</p>
-                    <p>{business.city}</p>
-                    <p>{business.state} {business.zipCode}</p>
-                </div>
-                <div className="Business-reviews">
-                    <h3>{business.category}</h3>
-                    <h3 className="rating">{business.rating} stars</h3>
-                    <p>{business.reviewCount} reviews</p>
+                    <div className="Business-address">
+                        <p>{business.address}</p>
+                        <p>{business.city}</p>
+                        <p>{`${business.state} ${business.zipCode}`}</p>
+                    </div>
+                    <div className="Business-reviews">
+                        <h3>{business.category.toUpperCase()}</h3>
+                        <h3 className="rating">{`${business.rating} stars`}</h3>
+                        <p>{`${business.reviewCount} reviews`}</p>
+                    </div>
                 </div>
             </div>
         );
