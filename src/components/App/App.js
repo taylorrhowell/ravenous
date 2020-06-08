cf6d7d65698acd40d7fb1242752c3945586b6520
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-06-08 11:21:13 -0700

    update App.css, import Comps to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
