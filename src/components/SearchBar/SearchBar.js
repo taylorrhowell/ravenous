f3ca741ca063ba8b1ee3d25f2ee85cf9dddad651
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-06-08 11:10:03 -0700

    build mthd in SearchBar to help the render mthd

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,5 +8,10 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
-    
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>
+        });
+    }
 }
