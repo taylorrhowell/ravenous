f47ff66f8865826c7504230d751c8b23fb7b47d2
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-06-08 10:29:27 -0700

    complete and export BusinessList Comp

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,6 +4,17 @@ import Business from '../Business/Business';
 
 class BusinessList extends React {
     render() {
-        
+        return (
+            <div className="BusinessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div>
+        );
     }
-}
+};
+
+export default BusinessList;
