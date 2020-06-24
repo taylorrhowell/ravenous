052f8c2d0d3005c7303aef8489c291839ac27f64
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-06-23 21:56:16 -0700

    add  visual feedback and simulate query

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,11 +18,14 @@ const business = {
 let businesses = [business, business, business, business, business, business]
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log('searching Yelp with ' + term + ', ' + location + ', and ' + sortBy);
+  }
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses}/>
       </div>
     );
